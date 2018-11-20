84bfd62c5dab3a85748665ca19aedde3ec951aea
BulgakViktoriya14 <bulgak.viktoriya@gmail.com>
2018-11-20 16:18:09 +0300

    modified seleniumwebdriver

--- seleniumwebdriver/src/test/java/SeleniumWebDriver.java
+++ seleniumwebdriver/src/test/java/SeleniumWebDriver.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -12,13 +13,12 @@ public class SeleniumWebDriver {
     public void findTicketWhenFieldToIsEmpty() {
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        WebDriverWait wait = new WebDriverWait(driver, 10);
         driver.get("https://www.britishairways.com/ru-us/flights-and-holidays/flights");
-        WebElement buttonFindFlights = driver.findElement(By.id("flightSearchButton"));
-        buttonFindFlights.click();
-        WebElement result = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".errorList>li")));
-        String realError = result.getText();
-        String expectedError = "Добавьте пункт назначения.";
+        driver.findElement(By.id("planTripFlightDestination")).clear();
+        new Actions(driver).moveToElement(driver.findElement(By.cssSelector(".styledRadioButton>input"))).click().build().perform();
+        driver.findElement(By.id("flightSearchButton")).click();
+        String realError = driver.findElement(By.cssSelector(".errorList>li")).getText();
+        String expectedError = "Please add the destination you're travelling 'To'.";
         driver.quit();
         assertEquals(realError,expectedError);
     }
